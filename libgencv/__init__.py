2a0782671f462b5681c2b3dd46cd624f34c9251e
matt1432 <matt@nelim.org>
2025-05-04 21:01:02 -0400

    feat: expose data classes

--- libgencv/__init__.py
+++ libgencv/__init__.py
@@ -1,3 +1,5 @@
 from . import collections_fix as collections_fix
+from .edition import Edition as Edition
 from .libgen_search import LibgenSearch as LibgenSearch
+from .result_file import ResultFile as ResultFile
 from .search_request import SearchRequest as SearchRequest
